ea49686bd3f71a99dcbf52578a3b34c04717da6c
Henrik <hb@enovate.de>
2014-12-04 22:07:05 +0100

    allow angular access-control

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,9 @@ module RilaRailsSql
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :de
     config.assets.initialize_on_precompile = false if ENV['BUILDPACK_RUNNING']
+    config.action_dispatch.default_headers.merge!({
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => '*'
+    })
   end
 end
