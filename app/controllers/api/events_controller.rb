32f88c3318e1b3cbde65651d0f8aece829056b66
Henrik <hb@enovate.de>
2014-12-11 20:33:44 +0100

    api acl

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -4,6 +4,9 @@ class API::EventsController < ApplicationController
 
   before_action :set_event, only: [:show, :edit, :update, :destroy, :select_run]
 
+  after_action :access_control_headers
+
+
   def index
     @events = Event.all
   end
@@ -11,53 +14,33 @@ class API::EventsController < ApplicationController
   def show
   end
 
-  # GET /events/new
   def new
     @event = Event.new
   end
 
-  # GET /events/1/edit
   def edit
   end
 
-  # POST /events
-  # POST /events.json
   def create
     @event = Event.new(event_params)
-
-    respond_to do |format|
-      if @event.save
-        format.html { redirect_to @event, notice: 'Event was successfully created.' }
-        format.json { render :show, status: :created, location: @event }
-      else
-        format.html { render :new }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
+    if @event.save
+      render :show, status: :created, location: @event
+    else
+      render json: @event.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /events/1
-  # PATCH/PUT /events/1.json
   def update
-    respond_to do |format|
-      if @event.update(event_params)
-        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
-        format.json { render :show, status: :ok, location: @event }
-      else
-        format.html { render :edit }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
+    if @event.update(event_params)
+      render :show, status: :ok, location: @event
+    else
+      render json: @event.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /events/1
-  # DELETE /events/1.json
   def destroy
     @event.destroy
-    respond_to do |format|
-      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    head :no_content
   end
 
   def select_run
@@ -65,17 +48,18 @@ class API::EventsController < ApplicationController
   end  
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_event
-      @event = Event.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def event_params
-      params.require(:event).permit(:name, :event_date)
-    end
+  def set_event
+    @event = Event.find(params[:id])
+  end
 
-    def format_json?
-      request.format.json?
-    end
+  def event_params
+    params.require(:event).permit(:name, :event_date)
+  end
+    
+  def set_access_control_headers
+    eaders['Access-Control-Allow-Origin'] = "*"
+    headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
+  end
+  
 end
