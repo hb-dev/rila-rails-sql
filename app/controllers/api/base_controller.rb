d1b9cd5d5a3937bb7380dddeef53334323a526aa
Henrik <hb@enovate.de>
2014-12-11 21:09:08 +0100

    api acl

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -18,7 +18,7 @@ class API::BaseController < ApplicationController
       headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
       headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
       headers['Access-Control-Max-Age'] = '1728000'
-
+      head(:ok)
       render :text => '', :content_type => 'text/plain'
     end
   end
